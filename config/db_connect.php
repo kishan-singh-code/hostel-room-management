d5a7f8571e5c7dfb3cbd46c2d7a1d0d41d5b0536
kishan-singh-code <kishansingh3d21f@gmail.com>
2021-09-03 11:28:32 +0530

    for remote sql

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
-	// connect to the database
-	$conn = mysqli_connect('localhost', 'kishan', '1234', 'hostel1');
-
-	// check connection
-	if(!$conn){
-		echo 'Connection error: '. mysqli_connect_error();
-	}
-
-?>
+// connect to the database
+//for localhost
+// $conn = mysqli_connect('localhost', 'kishan', '1234', 'hostel1');
+//for remote sql
+$conn = mysqli_connect('remotemysql.com', 'oXjGEcnwxa', 'DMO5aCywGt', 'oXjGEcnwxa');
+// check connection
+if (!$conn) {
+	echo 'Connection error: ' . mysqli_connect_error();
+}
